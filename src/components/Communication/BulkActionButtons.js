09923d9ecaebcaa3a2d95c801fb6d0b6176a21eb
nicolasdalmao <nicodalmao97@gmail.com>
2020-10-19 20:04:15 -0300

    fix: Se pueden borrar comunicados recurrentes ya publicados

--- src/components/Communication/BulkActionButtons.js
+++ src/components/Communication/BulkActionButtons.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { BulkDeleteButton, useListContext } from 'react-admin';
 import Tooltip from '@material-ui/core/Tooltip';
 
-import { IS_PUBLISHED } from './consts';
+import { DATE_TIME, IS_PUBLISHED } from './consts';
 
 const CommunicationsBulkActionButtons = (props) => {
   const { data, selectedIds } = useListContext();
   const selectedIdsContainPublishedComm = selectedIds.some(
-    (id) => data[id][IS_PUBLISHED]
+    (id) => data[id][IS_PUBLISHED] && !data[id][DATE_TIME]
   );
 
   return selectedIdsContainPublishedComm ? (
