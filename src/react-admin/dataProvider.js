ac7c58ae2323793cdafcbc902a09b1c22ad57dd3
nicolasdalmao <nicodalmao97@gmail.com>
2020-10-24 19:23:40 -0300

    feat: Se pueden filtrar y ordenar los comunicados

--- src/react-admin/dataProvider.js
+++ src/react-admin/dataProvider.js
@@ -4,14 +4,27 @@ async function deleteOneforEach(currentValue, index, array) {
   const id = currentValue;
   const resource = this;
   const url = `${resource}/${id}`;
-  const { json } = await httpClient(url, { method: 'DELETE' }); // JSON llega undefined
+  const { json } = await httpClient(url, { method: 'DELETE' });
   return { data: json };
 }
 
 export default {
   getList: async (resource, params) => {
     const { page, perPage } = params.pagination;
-    const url = `${resource}?page=${page}&per_page=${perPage}`;
+    const { field, order } = params.sort;
+    const { filter } = params;
+    const page_url = `page=${page}&per_page=${perPage}`;
+    const sort_url = `sort=["${field}","${order}"]`;
+    var filter_url = "";
+
+    // Agrego el filtro para cada campo de filter
+    const filter_fields = Object.keys(filter);
+    filter_fields.forEach(
+      (element) => {filter_url = filter_url + `&${element}=${filter[element]}`}
+    );
+
+    // Armo la url
+    const url = `${resource}?${page_url}&${sort_url}${filter_url}`;
     try {
       const { json } = await httpClient(url);
       return { data: json[resource], total: json.pagination.count };
